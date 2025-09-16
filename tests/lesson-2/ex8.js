815e4057b0ddc83f52331efdaab3d94dfebdd095
PW-Hien <dothanhhien2409@gmail.com>
2025-09-16 16:08:17 +0700

    chored: update answer for ex8

--- tests/lesson-2/ex8.js
+++ tests/lesson-2/ex8.js
@@ -1,5 +1,10 @@
-for (i = 1; i < 100; i+=4) {
+for (i = 1; i < 100; i++ +4) {
     if (i % 4 === 1) {
         console.log("Số cách nhau 4 đơn vị là", i)
     }
 }
+// for (i = 1; i < 100; i+=4) {
+//     if (i % 4 === 1) {
+//         console.log("Số cách nhau 4 đơn vị là", i)
+//     }
+// }
