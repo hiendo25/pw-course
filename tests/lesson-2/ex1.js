2b1c045194935979a0080329f1f08a10382d74e1
PW-Hien <dothanhhien2409@gmail.com>
2025-09-16 14:46:08 +0700

    chored: update answer

--- tests/lesson-2/ex1.js
+++ tests/lesson-2/ex1.js
@@ -1,9 +1,8 @@
 const number = 12;
 let name = "my number";
 var isEven = false;
-const result = number % 2;
 
-if (result < 1) {
+if (number % 2 < 1) {
     var isEven = true;
     console.log(isEven);
 }
--- tests/lesson-2/ex8.js
+++ tests/lesson-2/ex8.js
@@ -1,5 +1,5 @@
-for (i = 1; i < 100; i++) {
-    if (i % 4 == 1) {
+for (i = 1; i < 100; i+=4) {
+    if (i % 4 === 1) {
         console.log("Số cách nhau 4 đơn vị là", i)
     }
 }
